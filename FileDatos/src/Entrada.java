176647cc6aa2c3efe6960330d007480cb037e45b
MariahrrzCrg <85486107+MariahrrzCrg@users.noreply.github.com>
2021-10-01 12:47:27 +0200

    prueba

--- FileDatos/src/Entrada.java
+++ FileDatos/src/Entrada.java
@@ -1,5 +1,51 @@
+import java.io.*;
+
+
+
+
+
 public class Entrada {
-    public static void main(String[] args) {
-        System.out.println("Hola");
+
+
+    public
+    static void main(String[] args) {
+
+
+        //String
+
+
+        String dir ="C:\\DAM2PROYECTOS";
+
+
+        File
+                f = new File(dir);
+
+
+        String[]
+                archivos = f.list();
+
+
+        System.out.printf("Ficheros en el directorio actual: %d %n", archivos.length);
+
+
+        for
+        (int i = 0; i < archivos.length; i++) {
+
+
+            File
+                    f2 = new File(f, archivos[i]);
+
+
+            System.out.printf("Nombre: %s, es fichero?: %b, es directorio?: %b %n", archivos[i], f2.isFile(),
+
+
+                    f2.isDirectory());
+
+
+        }
+
+
     }
+
+
 }
